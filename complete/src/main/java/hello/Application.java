7205ef6e126764f556e5526a39cd99ec3549d8d1
Greg Turnquist <gturnquist@gopivotal.com>
2014-02-25 10:35:57 -0500

    Polish up code so it works nicely with Spring Boot

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+import hello.wsdl.GetCityForecastByZIPResponse;
+
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		WeatherClient weatherClient = ctx.getBean(WeatherClient.class);
+
+		String zipCode = "94304";
+		if (args.length > 0) {
+			zipCode = args[0];
+		}
+		GetCityForecastByZIPResponse response = weatherClient.getCityForecastByZip(zipCode);
+		weatherClient.printResponse(response);
+	}
+
+}
