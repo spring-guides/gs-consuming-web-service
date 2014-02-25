5d138ce0180f49160952a6274c797ad1e2a8de86
Greg Turnquist <gturnquist@gopivotal.com>
2014-02-25 10:36:29 -0500

    Fix bug in configuration

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -8,7 +8,7 @@ import hello.wsdl.GetCityForecastByZIPResponse;
 public class Application {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(WeatherConfiguration.class, args);
 
 		WeatherClient weatherClient = ctx.getBean(WeatherClient.class);
 
