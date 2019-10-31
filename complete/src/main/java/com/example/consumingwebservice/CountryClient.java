bc5b057095c2e3d063a513d35035fbb387239001
Jay Bryant <jbryant@pivotal.io>
2019-10-31 11:03:11 -0500

    Use the Spring Initializr (and Boot 2.2)
    I rewrote the document and the code (mostly changing package names) to use the Spring Initializr. I also modified the build files to use Spring Boot 2.2.0, which necessitated replacing the Gradle wrappers. Note that there are no tests (other than the one I pulled in from the src directory that the Initializr created).

--- complete/src/main/java/com/example/consumingwebservice/CountryClient.java
+++ complete/src/main/java/com/example/consumingwebservice/CountryClient.java
@@ -1,5 +1,5 @@
 
-package hello;
+package com.example.consumingwebservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,8 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
-import hello.wsdl.GetCountryRequest;
-import hello.wsdl.GetCountryResponse;
+import com.example.consumingwebservice.wsdl.GetCountryRequest;
+import com.example.consumingwebservice.wsdl.GetCountryResponse;
 
 public class CountryClient extends WebServiceGatewaySupport {
 
