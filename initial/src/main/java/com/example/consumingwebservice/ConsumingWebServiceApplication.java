ddd9fa4a1d28d8b649ca17b73034f3ec70b76d87
mskakici <133224700+mskakici@users.noreply.github.com>
2025-04-09 20:21:48 +0200

    upitno gotovo

--- initial/src/main/java/com/example/consumingwebservice/ConsumingWebServiceApplication.java
+++ initial/src/main/java/com/example/consumingwebservice/ConsumingWebServiceApplication.java
@@ -1,13 +1,30 @@
 package com.example.consumingwebservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.consumingwebservice.wsdl.GetCountryResponse;
 
 @SpringBootApplication
 public class ConsumingWebServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumingWebServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumingWebServiceApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner lookup(CountryClient countryClient) {
+    return args -> {
+      String country = "Spain";
+
+      if (args.length > 0) {
+        country = args[0];
+      }
+      GetCountryResponse response = countryClient.getCountry(country);
+      System.err.println(response.getCountry().getCurrency());
+    };
+  }
 
 }
